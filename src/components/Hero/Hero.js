3e4f1cf6cb93b8dc6e1d6526903495a0a8405edc
navidabdi <artsanaat@gmail.com>
2022-06-13 07:56:46 +0300

    add some style to the project

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,7 +3,7 @@ import './Hero.css';
 
 const Hero = () => {
   return (
-    <section className="container hero d-flex-center">
+    <section className="hero d-flex-center">
       <h1>Crypto Tracker</h1>
       <p>Get All The Info Regarding Your Favorite Crypto Currency</p>
     </section>
